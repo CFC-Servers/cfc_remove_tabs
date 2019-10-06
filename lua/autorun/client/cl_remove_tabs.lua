83374990403425f9e51f093963c0791a10b9b1e4
Brandon Sturgeon <sturgeonb4@gmail.com>
2019-10-06 17:21:32 -0400

    Various spacing fixes

--- lua/autorun/client/cl_remove_tabs.lua
+++ lua/autorun/client/cl_remove_tabs.lua
@@ -6,9 +6,9 @@ local function cfcShouldRemoveTab( tab )
 	local gp = language.GetPhrase
 	local tabText = tab:GetText()
 	
-	if tabText == gp("spawnmenu.category.dupes") then return true end
-	if tabText == gp("spawnmenu.category.saves") then return true end	
-	if tabText == gp("spawnmenu.category.npcs") and !LocalPlayer():IsAdmin() then return true end
+	if tabText == gp( "spawnmenu.category.dupes" ) then return true end
+	if tabText == gp( "spawnmenu.category.saves" ) then return true end	
+	if tabText == gp( "spawnmenu.category.npcs" ) and !LocalPlayer():IsAdmin() then return true end
 
 	return false
 end
@@ -20,12 +20,12 @@ local function cfcCheckTabs()
     end
 end
 
-hook.Remove("OnSpawnMenuOpen", "cfcSpawnMenuWhitelist")
-hook.Add( "OnSpawnMenuOpen", "cfcSpawnMenuWhitelist", cfcCheckTabs)
+hook.Remove( "OnSpawnMenuOpen", "cfcSpawnMenuWhitelist" )
+hook.Add( "OnSpawnMenuOpen", "cfcSpawnMenuWhitelist", cfcCheckTabs )
 
 -- This extra pass ensures that all tabs are removed.
 -- The Saves tab seems to be loaded in async so it wouldn't disappear until the second menu load
-hook.Remove("OnSpawnMenuOpen", "cfcSpawnMenuWhitelist-delayed")
+hook.Remove( "OnSpawnMenuOpen", "cfcSpawnMenuWhitelist-delayed" )
 hook.Add( "OnSpawnMenuOpen", "cfcSpawnMenuWhitelist-delayed", function()
-	timer.Simple(0, cfcCheckTabs)
-end)
+	timer.Simple( 0, cfcCheckTabs )
+end )
